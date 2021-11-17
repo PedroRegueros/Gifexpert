83aae1b115ea2cba197493da33975d859559939d
pedro.regueros <pedro.regueros@pragma.com.co>
2021-11-17 10:27:23 -0500

    Primer Commit

---
+++ docs/precache-manifest.6bd5e466a496639c147eff58926363ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f55397a7c2ca25e72cd2989dc92bde1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4e86493892bda49df54",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e86493892bda49df54",
+    "url": "/static/js/main.becf6ab1.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
